d6341ec6928f9e45e0fab7fffedebd861e61aaa7
Tornike Melikishvili <tornikevasiko@gmail.com>
2024-01-08 18:48:06 +0400

    any

--- Front-end/src/environments/environment.ts
+++ Front-end/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    apiEndpoint: 'https://chat-n4lhfwxuy-nnop55s-projects.vercel.app/',
+    apiEndpoint: 'https://chat-app-six-tawny.vercel.app/',
     accessToken: "BuAMkZ9rbqVLVineu6bt1GVT"
 };
